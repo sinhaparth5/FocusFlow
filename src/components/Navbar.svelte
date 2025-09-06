fb81d44bd2c3527b24ecad4b4e97624b7b1ebf15
Parth Sinha <sinhaparth555@gmail.com>
2025-09-06 17:39:30 +0100

    updated navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,18 @@
 <nav class="bg-[#dbdbdb]">
   <div class="container mx-auto px-4 py-2 flex justify-between items-center">
     <div class="text-[2rem] text-[#303030] font-display">
-      <a href="/" aria-label="Home page button and logo">FocusFlow</a>
+      <a href="/" aria-label="Home page button and logo" class="font-bold">FocusFlow</a>
     </div>
-    <div class="">
+    <div class="flex gap-4">
+      <div class="flex gap-4 text-[#303030] font-bold">
+        <a href="#about">About</a>
+        <a href="#features">Features</a>
+        <a href="#contact">Contact</a>
+        <a href="#adhd">ADHD</a>
+      </div>
+      <div class="text-[#303030] font-bold">
+        <a href="/register" class="border-[3px] rounded-[100px] border-[#303030] px-4 py-2">Register</a>
+      </div>
     </div>
   </div>
 </nav>
